f40c8f637323492c273a951939b29bf40bee9409
mananmani1 <mananmani5577@gmail.com>
2023-03-03 12:51:37 +0500

    submitted

---
+++ md5_img.py
@@ -0,0 +1,41 @@
+import hashlib
+import os
+from flask import Flask, request
+
+upload_image = './upload'
+
+app = Flask(__name__)
+
+app.config['upload_image'] = upload_image
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file1' not in request.files:
+            return 'there is no file1 in form!'
+        file1 = request.files['file1']
+        if not allowed_file(file1.filename):
+            return 'invalid file type!'
+        path = os.path.join(app.config['upload_image'], file1.filename)
+        file1.save(path)
+        with open(path, 'rb') as f:
+            file_hash = hashlib.md5(f.read()).hexdigest()
+        return ("md5 hash of the given image : "+file_hash)
+
+    return '''
+    <h1>Upload new File</h1>
+    <form method="post" enctype="multipart/form-data">
+      <input type="file" name="file1">
+      <input type="submit">
+    </form>
+    '''
+
+
+
+if __name__ == "__main__":
+    app.run()
